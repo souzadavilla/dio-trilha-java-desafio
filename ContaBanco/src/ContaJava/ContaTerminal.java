16add729c4b324eb10e9a3598ab3386960dbd250
Kauã-souzaa <kauad5224@gmail.com>
2025-12-15 19:53:45 -0300

    Meu 3 commit feito

--- ContaBanco/src/ContaJava/ContaTerminal.java
+++ ContaBanco/src/ContaJava/ContaTerminal.java
@@ -2,22 +2,21 @@ package ContaJava;
 
 import java.util.Scanner;
 
-public class ContaTerminal {
+ public class ContaTerminal {
 
-    public static void mensagem(] args) throws Exception {
+    public static void main(String [] args) throws Exception {
 
         Scanner scanner = new Scanner(System.in);
-
+        String nomeDoCliente = scanner.nextLine();
+        String agencia = scanner.nextLine();
         int numeroConta = scanner.nextInt();
         double saldo = scanner.nextDouble();
-        scanner.nextLine(); 
-        String agencia = scanner.nextLine();
-        String nomeDoCliente = scanner.nextLine();
-
-        scanner.close();
-
         Usuario Usuario = new Usuario();
+        
+        Usuario.mensagem(nomeDoCliente, numeroConta, saldo, agencia);
+         scanner.close();
 
+        
        
     }
 } 
